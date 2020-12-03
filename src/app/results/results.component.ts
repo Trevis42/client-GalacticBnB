ec5fc0ba303df022c240a2d93e15b7e9f8283465
Trevis42 <sirtrevis42@gmail.com>
2020-12-03 16:03:15 -0600

    moved buttons
    got ride of margins in prep for grid spacing

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -18,6 +18,7 @@ export class ResultsComponent implements OnInit {
   name = 'Angular';
   selection: string;
   price_expand: boolean = false;
+  showTop: boolean = false;
   constructor() {}
   public place : string;
 
